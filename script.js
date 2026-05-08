c4e99956b80cae05a716093886a03954a1ad692f
kvniranwb <niranjanvt967@gmail.com>
2026-05-08 21:55:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function getFormvalue() {
-    //Write your code here
+// your JS code here
 
+function getFormvalue() {
+  event.preventDefault();
+
+  let fname = document.querySelector('input[name="fname"]').value.trim();
+  let lname = document.querySelector('input[name="lname"]').value.trim();
+
+  alert(fname + " " + lname);
 }
